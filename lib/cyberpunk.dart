9babb89c8dd992b267ba6fe1b0443380d00bdceb
Abhay Kaushik <abhay.gyanbharati@gmail.com>
2020-06-29 03:21:22 +0530

    Add scrolling background
    Scrolling background added using the scrolling_sprite flutter
    package. Some of the sprites have been added to an image folder as
    flame looks for sprites in the 'images' folder by default.

--- lib/cyberpunk.dart
+++ lib/cyberpunk.dart
@@ -2,13 +2,28 @@ import 'dart:ui';
 import 'package:flame/game.dart';
 import 'package:flutter/semantics.dart';
 
-class CyberPunk extends Game { // Game class from Flame's game library
+const COLOR = const Color(0xFFAC204F);
+const SIZE = 52.0;
+const GRAVITY = 400.0;
+const JUMP = -300.0;
+
+class CyberPunk extends Game {
+  // Game class from Flame's game library
 
   Size screenSize;
   double tileSize;
 
+  // CyberPunk() {
+  //   initialize();
+  // }
+
+  // void initialize async{
+  //   resize(await Flame.util.initialDimensions);
+  // }
+
   @override
-  void render(Canvas canvas) { // Canvas class requires dart ui library
+  void render(Canvas canvas) {
+    // Canvas class requires dart ui library
     //Note: Render bottom-up as later renders come on top
     // Render background
     Rect bgRect = Rect.fromLTWH(0, 0, screenSize.width, screenSize.height);
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,145 @@
+// import 'package:flutter/material.dart';
+
+// import 'package:flame/util.dart';
+// import 'package:flutter/services.dart';
+
+// import 'package:cyberpunk_runner/cyberpunk.dart';
+
+// void main() {
+//   //async added to use await keyword
+//   WidgetsFlutterBinding.ensureInitialized();
+
+//   CyberPunk game = CyberPunk();
+//   runApp(game.widget);
+
+//   // Create an instance of the util class
+//   Util flameUtil = Util();
+//   //await keyword added as these functions return Future
+//   flameUtil.fullScreen();
+//   flameUtil.setOrientation(DeviceOrientation
+//       .landscapeLeft); //DevideOrientation requires flutter/services.dart
+// }
+//---------------------------------------------------------------------------
+// import 'package:flutter/material.dart';
+// import 'package:flame/game.dart';
+// import 'package:flame/flame.dart';
+// import 'package:flame/components/animation_component.dart';
+// import 'package:flame_scrolling_sprite/flame_scrolling_sprite.dart';
+
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Flame.util.fullScreen();
+//   final size = await Flame.util.initialDimensions();
+//   runApp(MyGame(size).widget);
+// }
+
+// class MyGame extends BaseGame {
+//   final Size size;
+
+//   MyGame(this.size) {
+//     add(ScrollingSpriteComponent(
+//       scrollingSprite: ScrollingSprite(
+//         spritePath: 'desert/clouds.png',
+//         width: size.width,
+//         spriteDestWidth: 640,
+//         spriteDestHeight: 160,
+//         horizontalSpeed: -50,
+//       ),
+//       y: 40,
+//     ));
+
+//     add(ScrollingSpriteComponent(
+//       scrollingSprite: ScrollingSprite(
+//         spritePath: 'desert/mountains.png',
+//         width: size.width,
+//         spriteDestWidth: 640,
+//         spriteDestHeight: 160,
+//         height: size.height,
+//         horizontalSpeed: -100,
+//       ),
+//       y: size.height - 180,
+//     ));
+
+//     add(ScrollingSpriteComponent(
+//       scrollingSprite: ScrollingSprite(
+//         spritePath: 'desert/ground.png',
+//         width: size.width,
+//         spriteDestWidth: 320,
+//         spriteDestHeight: 160,
+//         horizontalSpeed: -300,
+//       ),
+//       y: size.height - 160,
+//     ));
+
+//     add(AnimationComponent.sequenced(
+//       192.0,
+//       64.0,
+//       'dino-run.png',
+//       8,
+//       textureWidth: 96.0,
+//       textureHeight: 32.0,
+//     )
+//       ..y = size.height - 65
+//       ..x = size.width / 2 - 96);
+
+//     add(ScrollingSpriteComponent(
+//       scrollingSprite: ScrollingSprite(
+//         spritePath: 'desert/foreground.png',
+//         width: size.width,
+//         spriteDestWidth: 1280,
+//         spriteDestHeight: 160,
+//         height: size.height,
+//         horizontalSpeed: -1500,
+//       ),
+//       y: size.height - 160,
+//     ));
+//   }
+
+//   @override
+//   Color backgroundColor() => const Color(0xFF73acb6);
+// }
+// ---------------------------------------------------------
+
 import 'package:flutter/material.dart';
+import 'package:flame/game.dart';
+import 'package:flame/flame.dart';
+import 'package:flame_scrolling_sprite/flame_scrolling_sprite.dart';
 
-import 'package:flame/util.dart';
-import 'package:flutter/services.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Flame.util.fullScreen();
+  final size = await Flame.util.initialDimensions();
+  runApp(MyGame(size).widget);
+}
 
-import 'package:cyberpunk_runner/cyberpunk.dart';
+class MyGame extends Game {
+  ScrollingSprite sprite;
+  final Size size;
 
-void main() { //async added to use await keyword
-  //WidgetsFlutterBinding.ensureInitialized();
+  MyGame(this.size) {
+    sprite = ScrollingSprite(
+      spritePath: 'far-buildings.png',
+      height: size.height,
+      width: size.width,
+      //height: size.height,
+      horizontalSpeed: -200,
+      spriteDestWidth: 600,
+      spriteDestHeight: 300,
+    );
+  }
 
-  CyberPunk game = CyberPunk();
-  runApp(game.widget);
+  @override
+  void update(double dt) {
+    sprite.update(dt);
+  }
 
-  // Create an instance of the util class
-  Util flameUtil = Util(); 
-  //await keyword added as these functions return Future
-  flameUtil.fullScreen();
-  flameUtil.setOrientation(DeviceOrientation.landscapeRight); //DevideOrientation requires flutter/services.dart 
+  @override
+  void render(Canvas canvas) {
+    if (sprite.loaded()) {
+      sprite.renderAt(0, 100, canvas);
+    }
+  }
+
+  @override
+  Color backgroundColor() => const Color(0xFF052c46);
 }
