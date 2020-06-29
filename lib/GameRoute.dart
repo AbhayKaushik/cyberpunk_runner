2264da11244fbeeb3ea92fba2b51d23fc10b29b5
Kunal Raghav <kraghav123@gmail.com>
2020-06-29 16:34:04 +0530

    fixed routes for good

--- lib/GameRoute.dart
+++ lib/GameRoute.dart
@@ -1,27 +1,35 @@
 import 'package:cyberpunk_runner/cyberpunk.dart';
+import 'package:cyberpunk_runner/mygame.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class GameRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    CyberPunk cyberPunk = new CyberPunk();
+    Size size = MediaQuery.of(context).size;
+    MyGame cyberPunk = new MyGame(size);
     return WillPopScope(
       onWillPop: () async {
         return showDialog(
               context: context,
               builder: (context) => new AlertDialog(
                 title: new Text('Are you sure?'),
-                content: new Text('Do you want to exit an App'),
+                content: new Text('Do you want to exit the game'),
                 actions: <Widget>[
                   new GestureDetector(
                     onTap: () => Navigator.of(context).pop(false),
-                    child: Text("NO"),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text("NO"),
+                    ),
                   ),
                   SizedBox(height: 16),
                   new GestureDetector(
                     onTap: () => Navigator.of(context).pop(true),
-                    child: Text("YES"),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text("YES"),
+                    ),
                   ),
                 ],
               ),
